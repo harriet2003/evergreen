c6403f5d1eb9b128e7711021490b4610d6beb7eb
Harriet Eglinton <hattieeglinton@gmail.com>
2024-08-28 21:16:47 +1200

    Seedling Page 1 V1
    Going to move next button and progress bar into footer

--- plant-a-seed/plant1.php
+++ plant-a-seed/plant1.php
@@ -1,7 +1,3 @@
-<?php
-session_start();
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -12,18 +8,18 @@ session_start();
     <link rel="stylesheet" href="../css/styles.css" />
     <link rel="stylesheet" href="../css/typography.css" />
     <link rel="stylesheet" href="../css/navbar.css">
-    <script src="../js/script.js"></script>
+    <script src="../js/script.js" defer></script>
     <script src="https://kit.fontawesome.com/2028364a6f.js" crossorigin="anonymous"></script>
 </head>
 
 <body class="plantSeedlingPage">
     <header>
         <nav class="desktopNav">
-            <a href="ourForest.php" class="navTitle">Evergreen</a>
+            <a href="../ourForest.php" class="navTitle">Evergreen</a>
             <div>
-                <a href="ourForest.php">Our Forest</a>
+                <a href="../ourForest.php">Our Forest</a>
                 <a href="#">The Data</a>
-                <a href="about.html" class="currentPage">About</a>
+                <a href="../about.html" class="currentPage">About</a>
             </div>
         </nav>
 
@@ -46,35 +42,51 @@ session_start();
     <main>
         <h5 class="capitalise">Select a Seedling</h5>
 
+        <!--MOBILE-->
         <div class="mobileSeedlingSelection">
-            <i class="fa-solid fa-arrow-left"></i>
-            <div class="seedling">
-                <div>
-                    <img src="">
+            <div class="selectionBoxes">
+                <i class="fa-solid fa-arrow-left"></i>
+                <div class="seedling">
+                    <div>
+                        <img src="">
+                    </div>
+                    <h6>Mānuka</h6>
                 </div>
-                <h6>Mānuka</h6>
+                <i class="fa-solid fa-arrow-right"></i>
             </div>
-            <i class="fa-solid fa-arrow-right"></i>
+
+            <a href="plant2.php" class="button">Next</a>
         </div>
 
+        <!--DESKTOP-->
         <div class="desktopSeedlingSelection">
-            <i class="fa-solid fa-arrow-left"></i>
-            <div>
-                <img src="">
-                <h6>Pohutukawa</h6>
+            <div class="selectionBoxes">
+                <i class="fa-solid fa-arrow-left"></i>
+                <div class="seedlings">
+                    <div class="seedling">
+                        <div>
+                            <img src="">
+                        </div>
+                        <h6>Pohutukawa</h6>
+                    </div>
+                    <div class="seedling">
+                        <div>
+                            <img src="">
+                        </div>
+                        <h6>Mānuka</h6>
+                    </div>
+                    <div class="seedling">
+                        <div>
+                            <img src="">
+                        </div>
+                        <h6>Fern</h6>
+                    </div>
+                </div>
+                <i class="fa-solid fa-arrow-right"></i>
             </div>
-            <div>
-                <img src="">
-                <h6>Mānuka</h6>
-            </div>
-            <div>
-                <img src="">
-                <h6>Fern</h6>
-            </div>
-            <i class="fa-solid fa-arrow-right"></i>
-        </div>
 
-        <a href="plant2.php" class="button">Next</a>
+            <a href="plant2.php" class="button">Next</a>
+        </div>
 
         <div class="progressBar">
             <i class="fa-solid fa-circle currentCircle"></i>
@@ -82,6 +94,7 @@ session_start();
             <i class="fa-solid fa-circle"></i>
             <i class="fa-solid fa-circle"></i>
         </div>
+
 
     </main>
 </body>
